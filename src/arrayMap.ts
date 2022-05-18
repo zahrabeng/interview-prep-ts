e86bbe112cb5dff9a35828de7d2f738d8f708186
Zahra <bghzahra@outlook.com>
2022-05-19 00:41:36 +0100

    experimented with map and added tests

---
+++ src/arrayMap.ts
@@ -0,0 +1,27 @@
+/* 
+    the .map() method will go through each element in an array and 
+    update each element according to what you wish to do with it
+ */
+
+
+
+// using map function to times each number in the array by two
+export function timesTwo(array:number[]):number[]{
+    const doubledArr = array.map((num) => num * 2)
+    return doubledArr
+}
+
+
+//using map function to reformat an object
+
+const objArray = [{ key: 1, value: 10 },
+                { key: 2, value: 20 },
+                { key: 3, value: 30 }];
+
+
+export function objectReform (){
+    const reformattedObj = objArray.map(({key, value}) => ({[key]: value}))
+    return reformattedObj
+}
+
+console.log(objectReform())
---
+++ tests/arrayMap.test.ts
@@ -0,0 +1,7 @@
+import {timesTwo} from "../src/arrayMap"
+
+
+test("times each number in array by two", ()=>{
+    expect(timesTwo([1, 2, 3, 4, 5])).toStrictEqual([2,4,6,8,10])
+})
+
