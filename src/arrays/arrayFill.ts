da14d0addf6fb0c6de4000feeba2eb1195b8ba4f
Zahra <bghzahra@outlook.com>
2022-07-08 08:14:50 +0100

    added fill array method

---
+++ src/arrays/arrayFill.ts
@@ -0,0 +1,13 @@
+/* 
+    the .fill() method will change elements within
+    an array given a start index, end index and the 
+    value that you want to swap. 
+
+    .fill(value, start, end)
+ */
+
+
+//fills 1 from index 2 to index 4
+const likes = [1, 2, 3, 4, 5, 6, 7, 8]
+console.log(likes.fill(1, 2, 4))
+
